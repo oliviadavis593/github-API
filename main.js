b16797a8e1e17080d0e1363dcae36244687a8842
Olivia Davis <olivia.davis593@gmail.com>
2019-11-30 15:21:09 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Displays message when document has fully loaded
+$(document).ready(function() {
+    console.log('App loaded. Please submit something');
+    userInput();
+});
+
+//value that user puts in 
+function userInput() {
+  let search = $('#user-input');
+  return search;  
+}
+
+//Watching for the submit button
+function watchSubmitButton() {
+    $('#search-form').sumbit(e => {
+        e.preventDefault();
+        fetchUserName();
+    });
+}
+
+//Makes request to the Github API
+function fetchUserName() {
+    fetch('https://api.github.com/users/' + userInput() + '/repos')
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('I cannot find that. Try something else!'));
+}
+
+//Displays (renders) results to the DOM
+function displayResults(responseJson) {
+    console.log(responseJson);
+    
+}
